3ae798444d7e30dffde33e7fc40b0464be1ef3ea
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-04-10 18:28:49 +0530

    ADDED: Validate email address

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,16 +1,19 @@
-#!/bin/bash -x
+#!/bin/bash
 echo "---Welcome To User Registration---"
 
 #Constants declaration
-NamingPattern="^[A-Z][a-z]{3,20}$"
+NAMING_PATTERN="^[A-Z][a-z]{3,20}$"
+EMAIL_PATTERN="^[a-zA-Z0-9._a-zA-Z0-9]+@[a-zA-Z0-9.a-zA-Z0-9]+\.[A-Za-z]{2,4}$"
 
 #Variables declaration
 firstName=""
+lastName=""
+emailId=""
 
 #Taking Fist name as input from user and validate it
 read -p "Enter First Name : " firstName
 
-if [[ $firstName =~ $NamingPattern ]]
+if [[ $firstName =~ $NAMING_PATTERN ]]
 then
 	echo "Your First Name is Valid"
 else
@@ -20,9 +23,18 @@ fi
 #Last name validation
 read -p "Enter Last Name : " lastName
 
-if [[ $lastName =~ $NamingPattern ]]
+if [[ $lastName =~ $NAMING_PATTERN ]]
 then
 	echo "Your Last Name is Valid "
 else
 	echo "Your Last Name is Invalid!!!"
+fi
+
+#Email Id validation
+read -p "Enter email : " emailId
+if [[ $emailId =~ $EMAILPATTERN ]]
+then
+	echo "Valid email-Id"
+else
+	echo "Invalid email-Id"
 fi
