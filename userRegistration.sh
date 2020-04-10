8659a601521f58edb2048e8932117a7d285b5040
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-04-10 17:23:16 +0530

    ADDED: Validate user's first name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,2 +1,18 @@
-#!/bin/bash
+#!/bin/bash -x
 echo "---Welcome To User Registration---"
+
+#Constants declaration
+NamingPattern="^[A-Z][a-z]{3,20}$"
+
+#Variables declaration
+firstName=""
+
+#Taking name as input from user and validate it
+read -p "Enter first name : " firstName
+
+if [[ $firstName =~ $NamingPattern ]]
+then
+	echo "Valid Name"
+else
+	echo "Invalid name!!! Please enter first letter in capital."
+fi
